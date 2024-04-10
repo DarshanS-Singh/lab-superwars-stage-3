6753ff010a3575cd60f7b38da8dd8164e9531622
Darshan Singh <142567488+DarshanS-Singh@users.noreply.github.com>
2024-04-10 14:23:06 +0530

    Updatedd app.js

--- src/app.js
+++ src/app.js
@@ -21,25 +21,39 @@ const PLAYERS = [
     "Slingo"
 ];
 
+// initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = '';
+
+    // Instead of forloop use Map method
+    // Code here
+
     return detailedPlayers;
 }
 
+// getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
-const createPlayers = (players, type) => {
+// Build player template
+const buildPlayers = (players, type) => {
     let fragment = '';
+
+    // Instead of using for loop
+    // Use chaining of Array methods - filter, map and join
+    // Type your code here
+
     return fragment;
 }
 
-const checkPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = createPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = createPlayers(players, 'villain');
+// Display players in HTML
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
 
+
 window.onload = () => {
-    checkPlayers(initPlayers(PLAYERS));
+    viewPlayers(initPlayers(PLAYERS));
 }
